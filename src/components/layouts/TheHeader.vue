53d7baf9f4bcc30bd0e514b61d94254b5aa89653
zhanggexia <zgxia1221@163.com>
2020-05-16 16:25:40 +0800

    ces

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar navbar-default topnav">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">
+                    <span class="title">{{ logo.title }}</span>
+                    <img :src="logo.src" :alt="logo.title">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'TheHeader',
+    data(){
+        return{
+            logo: {
+                src: `${this.uploadsUrl}sitesByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+                title: `Learnku Vue.js`
+            }
+        }
+    },
+    beforeCreate(){
+        this.uploadsUrl='https://learnku.com/vuejs/uploads/'
+    }
+}
+</script>
+<style scoped>
+    .title {display:none;}
+</style>
