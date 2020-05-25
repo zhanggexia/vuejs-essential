319700c4605221306104693615feb426ef366bca
zhanggexia <zgxia1221@163.com>
2020-05-25 12:01:52 +0800

    first commit

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,31 +1,49 @@
 <template>
-    <div class="navbar navbar-default topnav">
-        <div class="container">
-            <div class="navbar-header">
-                <a href="/" class="navbar-brand">
-                    <span class="title">{{ logo.title }}</span>
-                    <img :src="logo.src" :alt="logo.title">
-                </a>
-            </div>
-        </div>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+
+      <div id="top-navbar-collapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
+
 <script>
 export default {
-    name:'TheHeader',
-    data(){
-        return{
-            logo: {
-                src: `${this.uploadsUrl}sitesByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
-                title: `Learnku Vue.js`
-            }
-        }
-    },
-    beforeCreate(){
-        this.uploadsUrl='https://learnku.com/vuejs/uploads/'
+  name: 'TheHeader',
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+        title: 'Learnku Vue.js'
+      },
+      navList: ['社区', '头条', '问答', '教程'],
+      activeNavIndex: 0
     }
+  },
+  beforeCreate() {
+    this.uploadsUrl = 'https://learnku.com/vuejs/uploads/'
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index
+    }
+  }
 }
 </script>
+
 <style scoped>
-    .title {display:none;}
+.title { display: none;}
+.navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
 </style>
