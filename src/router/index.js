f10b11750f1e981f70648a31726d05aa3c8e9aef
zhanggexia <zgxia1221@163.com>
2020-06-29 00:13:05 +0800

    编辑资料

--- src/router/index.js
+++ src/router/index.js
@@ -13,12 +13,15 @@ const router = new Router({
 
 // 全局前置守卫
 router.beforeEach((to, from, next) => {
-    const auth = router.app.$options.store.state.auth
-  
-    if (auth && to.path.indexOf('/auth/') !== -1) {
-      next('/')
-    } else {
-      next()
-    }
-  })
+  const auth = router.app.$options.store.state.auth
+
+  if (
+    (auth && to.path.indexOf('/auth/') !== -1) ||
+    (!auth && to.meta.auth)
+  ) {
+    next('/')
+  } else {
+    next()
+  }
+})
 export default router
